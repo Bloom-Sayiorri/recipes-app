bee7935cc5952f4d039a0983f730608bcd272072
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-07-24 18:15:48 +0300

    added an admin filed to users table

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_150149) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_24_151031) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_24_150149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.boolean "admin", default: false, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
